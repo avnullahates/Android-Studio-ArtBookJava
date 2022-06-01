5c21f4e69db3174a6c5b4b078b87e49fbf1788d2
avnullahates <pana__anka@hotmail.com>
2022-06-01 20:58:46 +0300

    User Art Book
    User can add picture and text on the database, and then user can see the database on the secreen

---
+++ ArtBookJava/app/src/main/java/com/ates/artbookjava/Art.java
@@ -0,0 +1,12 @@
+package com.ates.artbookjava;
+
+public class Art {
+    String name;
+    int id;
+
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
